2d28abcde17fc20892d54cd2229bd05edb0dcae3
Paulina Szywała <paulinaszywala01@gmail.com>
2021-12-02 11:42:34 +0100

    Initial commit

---
+++ src/Canva.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class Canva extends JPanel implements MouseInputListener {
+
+    public Canva()
+    {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10 ,5));
+
+        panel.setBackground(Color.LIGHT_GRAY);
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
